5b487f9a470c7e84dcc0a052c71ef9b56cb98da4
Ahmed ata abdallah <72627749+Ahmed-ata112@users.noreply.github.com>
2021-12-12 18:52:39 +0200

    added The Login window layout

--- Main_Program/lib/general_pages/login_page.dart
+++ Main_Program/lib/general_pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:getwidget/getwidget.dart';
 
 class login_page extends StatefulWidget {
   const login_page({Key? key}) : super(key: key);
@@ -8,10 +9,63 @@ class login_page extends StatefulWidget {
 }
 
 class _login_pageState extends State<login_page> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Text('HEY THERE')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'User Login',
+            style: TextStyle(
+                fontSize: 30, color: Colors.cyan, fontWeight: FontWeight.bold),
+          ),
+          Form(
+            key: _formKey,
+            child: SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.fromLTRB(20.0, 100.0, 20.0, 20.0),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    TextFormField(
+                      style: TextStyle(),
+                      decoration: InputDecoration(
+                          hintText: "username", icon: Icon(Icons.person)),
+                      onChanged: (val) {},
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      style: TextStyle(),
+                      decoration: InputDecoration(
+                          hintText: "Password", icon: Icon(Icons.password)),
+                      onChanged: (val) {},
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    ElevatedButton(
+                        child: Text(
+                          "LogIn",
+                          style: TextStyle(color: Colors.white, fontSize: 20.0),
+                        ),
+                        onPressed: () {}),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
