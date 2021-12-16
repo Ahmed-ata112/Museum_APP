e2d736cf34c8c16f9937051b2510ac521767d571
MennaTalhHossamAlden <76497207+MennaTalhHossamAlden@users.noreply.github.com>
2021-12-16 18:44:29 +0200

    accountant

---
+++ Main_Program/lib/general_pages/Accountant/SouvenirSale.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+class SouvenirSale extends StatelessWidget {
+  static String id='SouvenirSale';
+  @override
+  Widget build (BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder
+        (
+
+        ),
+    );
+  }
+}
