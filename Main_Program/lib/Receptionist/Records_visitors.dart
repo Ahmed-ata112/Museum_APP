95ac84a9086b505410b03542b2185b54990898a9
Doaa Magdy <71094316+doaamagdy2024@users.noreply.github.com>
2021-12-25 01:03:00 +0200

    changes in "Record_Visitors" file

--- Main_Program/lib/Receptionist/Records_visitors.dart
+++ Main_Program/lib/Receptionist/Records_visitors.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:main_program/Receptionist/Not_booked_yet.dart';
+import 'package:main_program/Receptionist/Already_booked.dart';
 
 class Records_Visitors extends StatefulWidget {
   const Records_Visitors({Key? key}) : super(key: key);
@@ -18,26 +20,31 @@ class _Records_VisitorsState extends State<Records_Visitors> {
       ),
       body: Center(
         child: Column(
-          children: <Widget>[
-            SizedBox(height: 200,),
-            RaisedButton(
-              onPressed: (){
-                //TODO --> MOVE TO ANOTHER PAGE
-              },
-              child: Text('Already Booked'),
-              color: Colors.blue[300],
-            ),
-            SizedBox(height: 100,),
-            RaisedButton(
-              onPressed: (){
-                //TODO --> MOVE TO ANOTHER PAGE
-              },
-              child:Text('Not Booked Yet'),
-              color:Colors.blue[300],
-            )
-          ]
+            children: <Widget>[
+              SizedBox(height: 200,),
+              RaisedButton(
+                onPressed: (){
+                  //TODO --> MOVE TO ANOTHER PAGE
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context)=> search_booking()));
+                },
+                child: Text('Already Booked'),
+                color: Colors.blue[300],
+              ),
+              SizedBox(height: 100,),
+              RaisedButton(
+                onPressed: (){
+                  //TODO --> MOVE TO ANOTHER PAGE
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context)=> new_booking()));
+                },
+                child:Text('Not Booked Yet'),
+                color:Colors.blue[300],
+              )
+            ]
         ),
       ),
     );
   }
 }
+
+
+
