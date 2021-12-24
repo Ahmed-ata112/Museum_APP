80a4c4c50ce31d9a1788be5a51fac671b5c7cb0d
Doaa Magdy <71094316+doaamagdy2024@users.noreply.github.com>
2021-12-25 01:04:23 +0200

    Adding coming tours & events

---
+++ Main_Program/lib/Receptionist/show_table_of_coming_tours_with_date.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class coming_tours extends StatefulWidget {
+  const coming_tours({Key? key}) : super(key: key);
+
+  @override
+  _coming_toursState createState() => _coming_toursState();
+}
+
+class _coming_toursState extends State<coming_tours> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
