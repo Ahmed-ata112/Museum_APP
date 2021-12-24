8d942beabf114b4a3e368a941e0931d503ff6ca1
Doaa Magdy <71094316+doaamagdy2024@users.noreply.github.com>
2021-12-24 21:14:32 +0200

    changes in "Already_booked" file

--- Main_Program/lib/Receptionist/Already_booked.dart
+++ Main_Program/lib/Receptionist/Already_booked.dart
@@ -11,36 +11,39 @@ class _search_bookingState extends State<search_booking> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Search for the ID '),
-      ),
+        appBar: AppBar(
+          title: Text('Search for the ID '),
+        ),
 
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            TextField(
+        body: Center(
+            child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
 
-              decoration: InputDecoration(
-              labelText: 'Search For The ID',
-              hintText: 'Enter Visitor ID',),
+                      decoration: InputDecoration(
+                        labelText: 'Search For The ID',
+                        hintText: 'Enter Visitor ID',),
 
-          onChanged:(text){ setState(() {
-            id = text;
-          });} ,
-            ),
-            SizedBox(height: 100),
-            RaisedButton(
-              onPressed: (){
-                //TODO
-                //ADD QUERY  --> SEARCH <SELECT> --> async
-                //then output a message
-              },
-              child: Text('Search')
-            ),
-          ]
+                      onChanged:(text){ setState(() {
+                        id = text;
+                      });} ,
+                    ),
+                  ),
+                  SizedBox(height: 100),
+                  RaisedButton(
+                      onPressed: (){
+                        //TODO
+                        //ADD QUERY  --> SEARCH <SELECT> --> async
+                        //then output a message
+                      },
+                      child: Text('Search')
+                  ),
+                ]
 
+            )
         )
-      )
 
     );
   }
